3872cf3f2c03b7c37bddc5757ddb0027b5a93f0e
Hans-Werner Hilse <hwhilse@gmail.com>
2015-05-15 01:00:12 +0200

    some adaptions

---
+++ config-linux.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+cd $(dirname "$0")/buildroot
+make $MAKE_OPTS BR2_EXTERNAL=$(pwd)/../buildroot-external linux-menuconfig
--- run.sh
+++ run.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 cd $(dirname "$0")
-source ./config
-
+sudo \
 qemu-system-x86_64 \
     -smp 2 \
     -m 128 \
     -nographic \
     -kernel bzImage \
-    -append "console=ttyS0" \
+    -initrd initramfs.cpio.xz \
+    -append "console=ttyS0 batmantestbedid=${ID:=1}" \
     -enable-kvm \
-    -netdev user,id=vmnic,hostname=batmantestbed1 \
-    -device virtio-net,netdev=vmnic \
+    -netdev tap,id=vmnic,ifname=battest${ID:=1},script=no,downscript=no \
+    -device virtio-net,netdev=vmnic,mac=ca:fe:42:00:00:$(printf "%02x" ${ID:=1}) \
     -object rng-random,filename=/dev/random,id=rng0
