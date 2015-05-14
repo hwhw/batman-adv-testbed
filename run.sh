3ad9330e1018962906f4816953d1927135d62dc5
Hans-Werner Hilse <hwhilse@gmail.com>
2015-05-15 01:42:38 +0200

    adapt qemu start script

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 cd $(dirname "$0")
-sudo \
+ID=$1
 qemu-system-x86_64 \
     -smp 2 \
-    -m 128 \
+    -m 64 \
     -nographic \
     -kernel bzImage \
     -initrd initramfs.cpio.xz \
@@ -11,4 +11,16 @@ qemu-system-x86_64 \
     -enable-kvm \
     -netdev tap,id=vmnic,ifname=battest${ID:=1},script=no,downscript=no \
     -device virtio-net,netdev=vmnic,mac=ca:fe:42:00:00:$(printf "%02x" ${ID:=1}) \
-    -object rng-random,filename=/dev/random,id=rng0
+    -serial telnet:localhost:$((55000+${ID:=1})),server,nowait \
+    -object rng-random,filename=/dev/random,id=rng0 \
+    -nodefaults \
+    -daemonize
+
+if [ ! -d /sys/class/net/battestbridge ]; then
+    brctl addbr battestbridge
+    ip a add 10.111.111.254/24 dev battestbridge
+    ip l set dev battestbridge up
+fi
+
+brctl addif battestbridge battest${ID:=1}
+ip l set dev battest${ID:=1} up
