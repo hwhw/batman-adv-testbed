d273a2e71f11233f30a7573da8f248c5d6530810
Hans-Werner Hilse <hwhilse@gmail.com>
2015-05-15 14:38:49 +0200

    switch to a squashfs based rootfs
    this won't need so much virtual RAM for unpacking the root fs

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cd $(dirname "$0")/buildroot
 make $MAKE_OPTS BR2_EXTERNAL=$(pwd)/../buildroot-external &&
-    cp output/images/bzImage ../bzImage &&
-    cp output/images/rootfs.cpio.xz ../initramfs.cpio.xz
+    cp output/images/bzImage ../ &&
+    cp output/images/rootfs.squashfs ../
--- run.sh
+++ run.sh
@@ -1,26 +1,18 @@
 #!/bin/bash
 cd $(dirname "$0")
-ID=$1
+
 qemu-system-x86_64 \
     -smp 2 \
     -m 64 \
     -nographic \
-    -kernel bzImage \
-    -initrd initramfs.cpio.xz \
-    -append "console=ttyS0 batmantestbedid=${ID:=1}" \
-    -enable-kvm \
-    -netdev tap,id=vmnic,ifname=battest${ID:=1},script=no,downscript=no \
-    -device virtio-net,netdev=vmnic,mac=ca:fe:42:00:00:$(printf "%02x" ${ID:=1}) \
-    -serial telnet:localhost:$((55000+${ID:=1})),server,nowait \
-    -object rng-random,filename=/dev/random,id=rng0 \
     -nodefaults \
-    -daemonize
-
-if [ ! -d /sys/class/net/battestbridge ]; then
-    brctl addbr battestbridge
-    ip a add 10.111.111.254/24 dev battestbridge
-    ip l set dev battestbridge up
-fi
-
-brctl addif battestbridge battest${ID:=1}
-ip l set dev battest${ID:=1} up
+    -enable-kvm \
+    -kernel bzImage \
+    -append "root=/dev/vda console=ttyS0 batmantestbedid=${ID:=1}" \
+    -net nic,vlan=0,model=virtio,macaddr=ca:fe:42:00:00:$(printf "%02x" ${ID:=1}) \
+    -net bridge,vlan=0,br=batmantestbr0 \
+    -object rng-random,filename=/dev/random,id=rng0 \
+    -drive file=rootfs.squashfs,if=virtio,format=raw,readonly \
+    ${OVERLAY:+-virtfs local,path="$OVERLAY",security_model=none,mount_tag=overlay} \
+    -serial ${SERIAL:-telnet:localhost:$((55000+${ID:=1})),server,nowait} \
+    $@
